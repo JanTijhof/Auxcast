c87940457defce4bfaa66bfd159f16b48827255e
JanTijhof <63103007+JanTijhof@users.noreply.github.com>
2023-01-02 20:50:10 +0100

    Add files via upload

---
+++ listen-for-cast.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides: listen-for-cast.py
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+### END INIT INFO
+
+# If you want a command to always run, put it here Carry out specific functions when asked
+
+# to by the system
+case "$1" in
+        start)
+                echo "Starting listen-for-cast.py"
+                /etc/auxcast/play-stream.py &
+                ;;
+        stop)
+                echo "Stopping listen-for-cast.py" pkill -f
+                /etc/auxcast/play-stream.py
+                ;;
+        *)
+                echo "Usage: /etc/init.d/listen-for-cast.sh {start|stop}" exit 1
+                ;;
+esac
+
+exit 0
