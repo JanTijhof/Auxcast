85ace5bac592eb37df45e681643ef8f455dd2fd2
JanTijhof <63103007+JanTijhof@users.noreply.github.com>
2023-01-02 21:13:19 +0100

    Update play-stream.py

--- play-stream.py
+++ play-stream.py
@@ -16,50 +16,39 @@ def setup():
 
 def loop():
 	while True:
-			if GPIO.event_detected(4):
-					print('Auxcast - Casting Button Pressed')
-					# Discover and connect to chromecasts named Muziek
-					chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=['Muziek'])
-					[cc.device.friendly_name for cc in chromecasts]
+		if GPIO.event_detected(4):
+				print('Auxcast - Casting Button Pressed')
+				# Discover and connect to chromecasts named Muziek
+				chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=['Muziek'])
+				[cc.device.friendly_name for cc in chromecasts]
 
-					cast = chromecasts[0]
-					# Start worker thread and wait for cast device to be ready
-					cast.wait()
-					print("Auxcast connected to:")
-					print(cast.device)
+				cast = chromecasts[0]
+				# Start worker thread and wait for cast device to be ready
+				cast.wait()
+				print("Auxcast connected to:")
+				print(cast.device)
 
-					mc = cast.media_controller
-					mc.play_media('http://192.168.1.169:8000/turntable.mp3', 'audio/mp3', stream_type="LIVE")
-					mc.block_until_active()
-					run_asap(mc, 0.1, 3)
-					#print(mc.status)
-
-#                       time.sleep(1)
-#                       mc.play()
-#                       time.sleep(0.2)
-#                       mc.play()
-#                       time.sleep(0.2)
-#                       mc.play()
-#                       time.sleep(0.2)
-#                       mc.play()
-
-
-					print('Auxcast - Connected, waiting for another button press')
+				mc = cast.media_controller
+				mc.play_media('http://192.168.1.169:8000/turntable.mp3', 'audio/mp3', stream_type="LIVE")
+				mc.block_until_active()
+				run_asap(mc, 0.1, 3)
+				#print(mc.status)
+				print('Auxcast - Connected, waiting for another button press')
 
 def run_asap(receiver, interval, blast_duration):
 	count = int(blast_duration/interval)
 	for i in range(count):
-			time.sleep(interval)
-			receiver.play()
+		time.sleep(interval)
+		receiver.play()
 
 if __name__ == '__main__':
 	setup()
 
 	try:
-			loop()
+		loop()
 
 	except KeyboardInterrupt:
-			GPIO.cleanup
-#               print ('Auxcast - script for castingbutton ended')
+		GPIO.cleanup
+#	print ('Auxcast - script for castingbutton ended')
 
 
